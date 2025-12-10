8ab2ff883a0ab3a3d2396def2b49566f64c35c1c
Mich-Seth Owusu <michsethowusu@gmail.com>
2025-12-10 12:49:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,40 @@
-"""
-Setup script for Terminex
-"""
-
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="terminex",
-    version="1.0.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="Terminology-aware translation system using Google Translate with controlled glossaries",
+    name="tc-translate",
+    version="0.1.0",
+    author="Mich-Seth Owusu",
+    author_email="michsethowusu@gmail.com",
+    description="Terminology-Controlled Translator with Google Translate API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/terminex",
+    url="https://github.com/GhanaNLP/tc-translate",
     packages=find_packages(),
+    package_data={
+        'tc_translate': ['terminologies/*.csv'],
+    },
+    include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Localization",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
     install_requires=[
-        "googletrans==4.0.0rc1",
+        "googletrans==4.0.0-rc1",
         "pandas>=1.3.0",
+        "click>=8.0.0",
     ],
-    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "tc-translate=tc_translate.cli:main",
+        ],
+    },
+    keywords="translation, terminology, google translate, localization",
 )
