50f30769a81b9d31d63a9756a0cfd593dd0d39be
Mich-Seth Owusu <michsethowusu@gmail.com>
2026-02-02 12:09:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,45 @@
+# setup.py
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
 setup(
-    name="ghana-translator",
-    version="0.1.0",
-    author="Mich-Seth Owusu",
-    author_email="michsethowusu@gmail.com",
-    description="Terminology-Controlled Translator with Google Translate API",
+    name="nkrane",
+    version="0.2.0",
+    author="GhanaNLP",
+    author_email="contact@ghananlp.org",
+    description="Enhanced Machine Translation with Terminology Control",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/GhanaNLP/ghana-translator",
+    url="https://github.com/GhanaNLP/nkrane",
     packages=find_packages(),
-    package_data={
-        'tc_translate': ['terminologies/*.csv'],
-    },
-    include_package_data=True,
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing :: Linguistic",
     ],
     python_requires=">=3.7",
-    install_requires=[
-        "googletrans @ git+https://github.com/michsethowusu/py-googletrans",
-        "pandas>=1.3.0",
-        "click>=8.0.0",
-    ],
+    install_requires=requirements,
+    include_package_data=True,
+    package_data={
+        "nkrane": ["sample_terminology.csv"],
+    },
     entry_points={
         "console_scripts": [
-            "ghana-translator=tc_translate.cli:main",
+            "nkrane=nkrane.cli:main",
         ],
     },
-    keywords="translation, terminology, google translate, localization",
 )
